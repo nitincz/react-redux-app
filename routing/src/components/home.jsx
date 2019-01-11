55d369c47102b57f1473ca615edce76c8c453aae
Nitin Ahuja <nitin@carezone.com>
2019-01-11 14:16:17 -0800

    Data loading with Axios

--- routing/src/components/home.jsx
+++ routing/src/components/home.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">
-                Home
-            </h4>
-            <p>
-                Hello, welcome to my home page!
-            </p>
-        </div>
-    );
+class Home extends React.Component {
+    state ={
+         posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+             .then(response => {
+                this.setState({
+                    posts: response.data.slice(0,10)
+                });
+             });
+    }
+    render() {
+
+        const { posts } = this.state;
+        const postsToRender = posts.map(
+            post => {
+                return (
+                    <div className="post card" key={ post.id}>
+                        <div className="card-content">
+                            <span className="card-title">
+                                {post.title}
+                            </span>
+                            <p>{ post.body }</p>
+                        </div>
+                    </div>
+                );
+            }
+        );
+        return posts.length == 0 ? (
+            <div className="center">Loading...</div>
+        ) : ( postsToRender );
+    }
 }
 
 export default Home
